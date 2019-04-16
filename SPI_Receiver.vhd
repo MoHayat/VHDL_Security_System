9828b30a28dfafad81de3f7825ae54d469686463
ATAlTamimi <48307481+ATAlTamimi@users.noreply.github.com>
2019-04-15 19:00:04 -0700

    Update SPI_Receiver.vhd

--- SPI_Receiver.vhd
+++ SPI_Receiver.vhd
@@ -14,9 +14,10 @@ entity SPI_Receiver is
     VSYNC_in           : in  std_logic;
 	HREF_in            : in  std_logic;
     -- Receiver Outputs
-    R_level_out        : out std_logic_vector(7 downto 0);
-    G_level_out        : out std_logic_vector(7 downto 0);
-    B_level_out        : out std_logic_vector(7 downto 0)
+    RGB_level_out        : out std_logic_vector(7 downto 0);
+    --G_level_out        : out std_logic_vector(7 downto 0);
+	--B_level_out        : out std_logic_vector(7 downto 0);
+	Read_EN_out        : out std_logic_vector(2 downto 0)
   );
 end SPI_Receiver;
 
@@ -30,9 +31,6 @@ architecture rtl of SPI_Receiver is
   ---------------------------------------------------------------------------
   --constants, types, functions
   ---------------------------------------------------------------------------
-  -- constant num_clocks_in_1_sec_c   : integer := 1 sec / (1 ns * 10); 
-  -- constant num_clocks_in_2_secs_c  : integer := num_clocks_in_1_sec_c * 2;  
-  -- constant num_clocks_in_3_secs_c  : integer := num_clocks_in_1_sec_c * 3;
   constant Column_max  : integer := 800 ;  
   constant Row_max     : integer := 600 ;
   
@@ -46,36 +44,15 @@ architecture rtl of SPI_Receiver is
   --------------------------------------------------------------------------- 
   signal Column_count_s        : integer range 0 to 799;
   signal Row_count_s           : integer range 0 to 599;
-  signal state_s               : row_state;
-  -- signal enable_100MHz         : std_logic;
-  -- signal clock_count_s         : integer range 0 to num_clocks_in_3_secs_c;
-  -- signal state_s               : red_light_state;
-  
--- begin
-  -- Slowclk: process(rst_in, clk_36mhz_in)
-  -- begin
-    -- if (rst_in = '1') then
-      -- clock100MHZ_count_s <= 0;
-      -- enable_100MHz       <= '0';
-    -- elsif rising_edge(clk_36mhz_in) then
-      -- if (clock100MHZ_count_s = 9) then
-        -- clock100MHZ_count_s <= 0;
-        -- enable_100MHz       <= '1';
-      -- else
-        -- clock100MHZ_count_s <= clock100MHZ_count_s + 1;
-        -- enable_100MHz       <= '0';
-      -- end if;
-    -- end if;
-  -- end process;  
-        
-          
+  signal state_s               : row_state; 
 
   RGB_sorter : process(rst_in, clk_36Mhz_in)
   begin
     if (rst_in = '1') then
-      R_level_out    <= (others => '0'); 
-      G_level_out    <= (others => '0');
-	  B_level_out    <= (others => '0');
+      RGB_level_out    <= (others => '0'); 
+      --G_level_out    <= (others => '0');
+	  --B_level_out    <= (others => '0');
+	  Read_EN_out    <= (others => '0');
 	  Column_count_s <= 0;
 	  Row_count_s    <= 0;	  
       state_s            <= even_row;
@@ -84,10 +61,12 @@ architecture rtl of SPI_Receiver is
 			case state_s is 
 				when even_row =>
 					if ((Column_count_s mod 2 = 0) and (Column_count_s /= Column_max) ) then
-						B_level_out <= RGB_level_in;
+						RGB_level_out <= RGB_level_in;
+						Read_EN_out    <= "100";
 						Column_count_s <= Column_count_s + 1;
 					elsif ((Column_count_s mod 2 = 1) and (Column_count_s /= Column_max) ) then
-						G_level_out <= RGB_level_in;
+						RGB_level_out <= RGB_level_in;
+						Read_EN_out    <= "010";
 						Column_count_s <= Column_count_s + 1;
 					else
 						Column_count_s <= 0;
@@ -97,10 +76,12 @@ architecture rtl of SPI_Receiver is
 			
 				when odd_row =>
 					if ((Column_count_s mod 2 = 0) and (Column_count_s /= Column_max) ) then
-						G_level_out <= RGB_level_in;
+						--RGB_level_out <= RGB_level_in;
+						Read_EN_out    <= (others => '0');
 						Column_count_s <= Column_count_s + 1;
 					elsif ((Column_count_s mod 2 = 1) and (Column_count_s /= Column_max) ) then
-						R_level_out <= RGB_level_in;
+						RGB_level_out <= RGB_level_in;
+						Read_EN_out    <= "001";
 						Column_count_s <= Column_count_s + 1;
 					else
 						Column_count_s <= 0;
